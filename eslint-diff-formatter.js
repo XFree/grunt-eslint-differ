aa38a604df8f26da7818cada3c21fbe2df87e342
Dmitriy Stepanov <dmitriy.stepanov@billing.ru>
2017-11-24 14:11:51 +0300

    refactor: разбиение на файлы

--- eslint-diff-formatter.js
+++ eslint-diff-formatter.js
@@ -1,43 +1,11 @@
 /* eslint-env es6*/
-module.exports = function (results) {
-  'use strict';
+const differ = require('./lib/differ');
+
+module.exports = (results) => {
   const path = require('path'),
         diffFilePath = process.env.ESLINT_DIFF_FORMATTER_FILE && process.env.ESLINT_DIFF_FORMATTER_FILE.trim(),
         diffFiles = diffFilePath ? require(path.resolve(diffFilePath)) : null,
-        messageEqualFields = ['ruleId', 'fatal', 'severity', 'source', 'message'],
-        severityFields = {1: 'warningCount', 2: 'errorCount'},
-        fixSeverityFields = {1: 'fixableWarningCount', 2: 'fixableErrorCount'},
-        {CLIEngine} = require('eslint'),
         htmlFormatter = CLIEngine.getFormatter('html');
 
-  let result = diffFiles ? results.reduce((result, file) => {
-    const diffFile = diffFiles.find((item) => {
-      return item.filePath === file.filePath;
-    });
-
-    file.messages = file.messages.filter((message) => {
-      const foundMessage = diffFile.messages.find((diffMessage) => {
-        return messageEqualFields.reduce((a, b) => {
-          return a && diffMessage[b] === message[b];
-        }, true);
-      });
-
-      if (foundMessage) {
-        file[severityFields[foundMessage.severity]]--;
-        if (foundMessage.fix) {
-          file[fixSeverityFields[foundMessage.severity]]--;
-        }
-      }
-
-      return !foundMessage;
-    });
-
-    if (file.messages.length) {
-      result.push(file);
-    }
-
-    return result;
-  }, []) : results;
-
-  return htmlFormatter(result);
+  return htmlFormatter(differ(results, diffFiles));
 };
